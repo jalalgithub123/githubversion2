7fdff52702f492e42890ba89730faace6b427da3
jalal8068 <jalalahamad9@gmail.com>
2019-04-10 12:42:20 +0530

    2nd jalal commit

--- src/com/sample/test/Sample2Test.java
+++ src/com/sample/test/Sample2Test.java
@@ -3,5 +3,6 @@ package com.sample.test;
 public class Sample2Test {
 public static void main(String[] args) {
 	System.out.println("Welcome to java jalal");
+	System.out.println("Welcome to java jalal1");
 }
 }
